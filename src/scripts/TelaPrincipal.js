f0edd2f45ef579be1f9772cac392dfe4e2608876
Mariana Dias Pereira <marianamkt6@gmail.com>
2023-06-22 11:10:15 -0300

    trabalhando na tela principal

---
+++ src/scripts/TelaPrincipal.js
@@ -0,0 +1,17 @@
+
+// Obtendo os dados armazenados na localStorage
+var dadosArmazenados = localStorage.getItem('dados');
+
+// Verificando se existem dados armazenados
+if (dadosArmazenados) {
+  // Convertendo os dados de volta para um objeto JavaScript (se necessário)
+  var dadosObjeto = JSON.parse(dadosArmazenados);
+
+  // Exibindo os dados na página
+  var elemento = document.getElementById('meuElemento');
+  elemento.textContent = dadosObjeto.nome; // Supondo que o objeto tenha uma propriedade 'nome'
+} else {
+  // Se não houver dados armazenados, exiba uma mensagem alternativa
+  var elemento = document.getElementById('meuElemento');
+  elemento.textContent = 'Nenhum dado armazenado na localStorage.';
+}
