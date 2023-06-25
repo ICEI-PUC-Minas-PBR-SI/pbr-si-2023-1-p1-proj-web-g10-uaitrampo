07ebce370ede61f5a340ca76bf5895e10ccd1bbb
lucasfelipeb <lucasfelipe96@hotmail.com>
2023-06-25 18:11:33 -0300

    Inclusão de localStorage

--- src/scripts/perfil.js
+++ src/scripts/perfil.js
@@ -1,32 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const fileInput = document.getElementById("formFileMultiple");
-    const divPortfolio = document.querySelector(".LinksPDF");
-    const adicionarBtn = document.querySelector(".botaoAdicionar button");
-    const excluirBtn = document.querySelector(".botoesTelaExcluirPortfolio .btn-outline-danger");
+  const fileInput = document.getElementById("formFileMultiple");
+  const divPortfolio = document.querySelector(".LinksPDF");
+  const adicionarBtn = document.querySelector(".botaoAdicionar button");
+  const excluirBtn = document.querySelector(".botoesTelaExcluirPortfolio .btn-outline-danger");
 
-    adicionarBtn.addEventListener("click", function() {
-      const files = fileInput.files;
-  
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-  
-        const fileLink = document.createElement("a");
-        fileLink.textContent = file.name;
-        fileLink.href = URL.createObjectURL(file);
-        fileLink.target = "_blank";
-  
-        divPortfolio.appendChild(fileLink);
-      }
-      
-      fileInput.value = "";
-    });
+  const storedLinks = localStorage.getItem("portfolioLinks");
+  if (storedLinks) {
+    divPortfolio.innerHTML = storedLinks;
+  }
 
-    function limparPortfolio() {
-      while (divPortfolio.firstChild) {
-        divPortfolio.removeChild(divPortfolio.firstChild);
-      }
+  adicionarBtn.addEventListener("click", function() {
+    const files = fileInput.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+
+      const fileLink = document.createElement("a");
+      fileLink.textContent = file.name;
+      fileLink.href = URL.createObjectURL(file);
+      fileLink.target = "_blank";
+
+      divPortfolio.appendChild(fileLink);
     }
 
-    // Adiciona um evento de clique ao botão "Excluir"
-    excluirBtn.addEventListener("click", limparPortfolio);
+    localStorage.setItem("portfolioLinks", divPortfolio.innerHTML);
+
+    fileInput.value = "";
+  });
+
+  function limparPortfolio() {
+    divPortfolio.innerHTML = "";
+    localStorage.removeItem("portfolioLinks");
+  }
+
+  excluirBtn.addEventListener("click", limparPortfolio);
 });
